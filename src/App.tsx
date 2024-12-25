b436345373c2b62999db86b3fb02449bc8490e32
natch <raphael.gonon18@gmail.com>
2024-12-25 23:47:21 +0100

    fix: build error
    Signed-off-by: natch <raphael.gonon18@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import batman from './assets/batman-logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div class="topnav">
+      <div>
         <a href="https://www.youtube.com/watch?v=k8m-00D0xj8" target="_blank">
           <img src={batman} className="logo batman" alt="Batman" />
         </a>
       </div>
       <h1>Batman Consulting</h1>
-      <div classname="info">
+      <div className="info">
         <p>
             Working hours 9pm to 9am.
         </p>
